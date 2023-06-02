677d5c268ad9f8e0ef5759c3ce55eac4f30110b5
Bilol665 <bilolsultan665@gmail.com>
2023-06-02 16:00:25 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/librarysecurity/LibrarySecurityApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.librarysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/librarysecurity/LibrarySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.librarysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
